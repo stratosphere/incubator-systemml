f5abd4c45ff60ab0f866a61eb5200e47f8716cdf
Christoph Bruecke <christoph.bruecke@campus.tu-berlin.de>
2016-03-03 15:51:12 +0100

    Implement MatrixScalarFLInstruction

---
+++ src/main/java/org/apache/sysml/runtime/instructions/flink/BinaryFLInstruction.java
@@ -0,0 +1,45 @@
+package org.apache.sysml.runtime.instructions.flink;
+
+import org.apache.sysml.runtime.DMLRuntimeException;
+import org.apache.sysml.runtime.instructions.InstructionUtils;
+import org.apache.sysml.runtime.instructions.cp.CPOperand;
+import org.apache.sysml.runtime.matrix.operators.Operator;
+
+/**
+ * Abstract class for all binary Flink instructions.
+ */
+public abstract class BinaryFLInstruction extends FLInstruction {
+
+    protected CPOperand input1, input2, output;
+
+    public BinaryFLInstruction(Operator op, CPOperand input1, CPOperand input2, CPOperand output, String opcode, String istr) {
+        super(op, opcode, istr);
+        this.input1 = input1;
+        this.input2 = input2;
+        this.output = output;
+    }
+
+    /**
+     *
+     * @param instr
+     * @param in1
+     * @param in2
+     * @param out
+     * @return
+     * @throws DMLRuntimeException
+     */
+    // TODO can potentially be moved to InstructionUtils
+    protected static String parseBinaryInstruction(String instr, CPOperand in1, CPOperand in2, CPOperand out)
+            throws DMLRuntimeException
+    {
+        String[] parts = InstructionUtils.getInstructionPartsWithValueType(instr);
+        InstructionUtils.checkNumFields(parts, 3);
+
+        String opcode = parts[0];
+        in1.split(parts[1]);
+        in2.split(parts[2]);
+        out.split(parts[3]);
+
+        return opcode;
+    }
+}
