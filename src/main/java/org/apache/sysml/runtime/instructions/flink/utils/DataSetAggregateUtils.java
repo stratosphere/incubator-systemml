a5cb2bd6a2af52be33ef69253e8ed4936ed38c8b
FelixNeutatz <neutatz@googlemail.com>
2016-04-19 13:21:25 +0200

    Fix AggBinary, RandInstruction and starting to fix MapMM

--- src/main/java/org/apache/sysml/runtime/instructions/flink/utils/DataSetAggregateUtils.java
+++ src/main/java/org/apache/sysml/runtime/instructions/flink/utils/DataSetAggregateUtils.java
@@ -36,7 +36,7 @@ import org.apache.sysml.runtime.matrix.operators.AggregateOperator;
 
 public class DataSetAggregateUtils {
 
-    public static DataSet<Tuple2<MatrixIndexes, MatrixBlock>> mergeByKey(
+	public static DataSet<Tuple2<MatrixIndexes, MatrixBlock>> mergeByKey(
             DataSet<Tuple2<MatrixIndexes, MatrixBlock>> input) {
         return input.groupBy(0).reduce(new MergeBlocksFunction());
     }
@@ -49,6 +49,56 @@ public class DataSetAggregateUtils {
             throw new DMLRuntimeException("Could not collect final block of " + input);
         }
     }
+
+	/**
+	 *
+	 * @param in
+	 * @return
+	 */
+	public static MatrixBlock sumStable1( DataSet<Tuple2<MatrixIndexes, MatrixBlock>> in ) {
+		try {
+			return (MatrixBlock) in.map(new DataSetConverterUtils.ExtractElement(1)).returns(MatrixBlock.class).reduce(
+					new SumSingleBlockFunction() ).collect().get(0);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+
+	/**
+	 *
+	 */
+	private static class ExtractMatrixBlock implements MapFunction<Tuple2<MatrixIndexes, CorrMatrixBlock>, MatrixBlock >
+	{
+		@Override
+		public MatrixBlock map(Tuple2<MatrixIndexes, CorrMatrixBlock> arg0)
+			throws Exception
+		{
+			return arg0.f1.getValue();
+		}
+	}
+
+	/**
+	 *
+	 * @param in
+	 * @return
+	 */
+	public static DataSet<Tuple2<MatrixIndexes, MatrixBlock>> sumByKeyStable( DataSet<Tuple2<MatrixIndexes, MatrixBlock>> in )
+	{
+		//stable sum of blocks per key, by passing correction blocks along with aggregates 		
+		DataSet<Tuple2<MatrixIndexes, CorrMatrixBlock>> tmp =
+			in.groupBy(0). new CreateBlockCombinerFunction(),
+				new MergeSumBlockValueFunction(),
+				new MergeSumBlockCombinerFunction() );
+
+		//strip-off correction blocks from 					     
+		DataSet<Tuple2<MatrixIndexes, MatrixBlock>> out =
+			tmp.map( new ExtractMatrixBlock() );
+
+		//return the aggregate rdd
+		return out;
+	}
 
     public static MatrixBlock aggStable(DataSet<Tuple2<MatrixIndexes, MatrixBlock>> in,
                                         AggregateOperator aop) throws DMLRuntimeException {
