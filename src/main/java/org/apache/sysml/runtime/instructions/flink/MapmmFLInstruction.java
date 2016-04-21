e2b5ef56483d1e9ee6d0c2fdc8dba3dd2061ca66
FelixNeutatz <neutatz@googlemail.com>
2016-04-21 23:00:18 +0200

    Add MapMM Spark-like implementation

--- src/main/java/org/apache/sysml/runtime/instructions/flink/MapmmFLInstruction.java
+++ src/main/java/org/apache/sysml/runtime/instructions/flink/MapmmFLInstruction.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
+ * 
  *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -18,84 +18,436 @@
  */
 
 package org.apache.sysml.runtime.instructions.flink;
+
+
+import org.apache.flink.api.common.functions.RichFlatMapFunction;
+import org.apache.flink.api.common.functions.RichMapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.sysml.hops.AggBinaryOp;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.util.Collector;
+
+import org.apache.sysml.runtime.controlprogram.context.FlinkExecutionContext;
+
+import org.apache.sysml.hops.AggBinaryOp.SparkAggType;
 import org.apache.sysml.lops.MapMult;
 import org.apache.sysml.lops.MapMult.CacheType;
 import org.apache.sysml.runtime.DMLRuntimeException;
 import org.apache.sysml.runtime.controlprogram.context.ExecutionContext;
-import org.apache.sysml.runtime.controlprogram.context.FlinkExecutionContext;
+import org.apache.sysml.runtime.functionobjects.Multiply;
+import org.apache.sysml.runtime.functionobjects.Plus;
 import org.apache.sysml.runtime.instructions.InstructionUtils;
 import org.apache.sysml.runtime.instructions.cp.CPOperand;
+import org.apache.sysml.runtime.instructions.flink.functions.FilterNonEmptyBlocksFunction;
+import org.apache.sysml.runtime.instructions.flink.functions.MatrixMultiplicationFunctions;
+import org.apache.sysml.runtime.instructions.flink.utils.DataSetAggregateUtils;
+import org.apache.sysml.runtime.matrix.MatrixCharacteristics;
 import org.apache.sysml.runtime.matrix.data.MatrixBlock;
 import org.apache.sysml.runtime.matrix.data.MatrixIndexes;
-import static org.apache.sysml.runtime.instructions.flink.functions.MatrixMultiplicationFunctions.*;
+import org.apache.sysml.runtime.matrix.data.OperationsOnMatrixValues;
+import org.apache.sysml.runtime.matrix.operators.AggregateBinaryOperator;
+import org.apache.sysml.runtime.matrix.operators.AggregateOperator;
+import org.apache.sysml.runtime.matrix.operators.Operator;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * Implementation of {@link org.apache.sysml.runtime.instructions.Instruction} to run matrix multiplication on Flink.
+ * TODO: we need to reason about multiple broadcast variables for chains of mapmults (sum of operations until cleanup) 
+ *
  */
-public class MapmmFLInstruction extends BinaryFLInstruction {
+public class MapmmFLInstruction extends BinaryFLInstruction
+{
 
-	private final MapMult.CacheType cacheType;
-	private final boolean outputEmtpy;
-	private final AggBinaryOp.SparkAggType aggType;
+	private CacheType _type = null;
+	private boolean _outputEmpty = true;
+	private SparkAggType _aggtype;
 
-	public MapmmFLInstruction(CPOperand input1, CPOperand input2, CPOperand output,
-							  CacheType cacheType, boolean outputEmtpy, AggBinaryOp.SparkAggType aggType,
-							  String opcode, String istr) {
-		super(null, input1, input2, output, opcode, istr);
-		this.cacheType = cacheType;
-		this.aggType = aggType;
-		this.outputEmtpy = outputEmtpy;
+	public MapmmFLInstruction(Operator op, CPOperand in1, CPOperand in2, CPOperand out, CacheType type,
+							  boolean outputEmpty, SparkAggType aggtype, String opcode, String istr )
+	{
+		super(op, in1, in2, out, opcode, istr);
+		_fltype = FLINSTRUCTION_TYPE.MAPMM;
+
+		_type = type;
+		_outputEmpty = outputEmpty;
+		_aggtype = aggtype;
 	}
 
 	/**
-	 * Factory method that parses an instruction string of the form
-	 * <p>
-	 * <pre><code>FLINK°mapmm°_mVar1·MATRIX·DOUBLE°_mVar2·MATRIX·DOUBLE°_mVar3·MATRIX·DOUBLE°RIGHT°false°MULTI_BLOCK</code></pre>
 	 *
-	 * and returns a new {@link MapmmFLInstruction} representing the instruction string.
-	 *
-	 * @param instr Instruction string, operands separated by <code>°</code> and value types by <code>·</code>
-	 * @return a new MapmmFLInstruction
+	 * @param str
+	 * @return
 	 * @throws DMLRuntimeException
 	 */
-	public static MapmmFLInstruction parseInstruction(String instr) throws DMLRuntimeException {
-		String parts[] = InstructionUtils.getInstructionPartsWithValueType(instr);
+	public static MapmmFLInstruction parseInstruction( String str )
+		throws DMLRuntimeException
+	{
+		String parts[] = InstructionUtils.getInstructionPartsWithValueType(str);
 		String opcode = parts[0];
-		if (opcode.equalsIgnoreCase(MapMult.OPCODE)) {
+
+		if ( opcode.equalsIgnoreCase(MapMult.OPCODE))
+		{
 			CPOperand in1 = new CPOperand(parts[1]);
 			CPOperand in2 = new CPOperand(parts[2]);
 			CPOperand out = new CPOperand(parts[3]);
-			MapMult.CacheType type = MapMult.CacheType.valueOf(parts[4]);
+			CacheType type = CacheType.valueOf(parts[4]);
 			boolean outputEmpty = Boolean.parseBoolean(parts[5]);
-			AggBinaryOp.SparkAggType aggtype = AggBinaryOp.SparkAggType.valueOf(parts[6]);
+			SparkAggType aggtype = SparkAggType.valueOf(parts[6]);
 
-			return new MapmmFLInstruction(in1, in2, out, type, outputEmpty, aggtype, opcode, instr);
-		} else {
+			AggregateOperator agg = new AggregateOperator(0, Plus.getPlusFnObject());
+			AggregateBinaryOperator aggbin = new AggregateBinaryOperator(Multiply.getMultiplyFnObject(), agg);
+			return new MapmmFLInstruction(aggbin, in1, in2, out, type, outputEmpty, aggtype, opcode, str);
+		}
+		else {
 			throw new DMLRuntimeException("MapmmSPInstruction.parseInstruction():: Unknown opcode " + opcode);
 		}
+
 	}
 
 	@Override
-	public void processInstruction(ExecutionContext ec) throws DMLRuntimeException {
-		assert ec instanceof FlinkExecutionContext :
-				"Expected " + FlinkExecutionContext.class.getCanonicalName() + " got " + ec.getClass().getCanonicalName();
-		FlinkExecutionContext fec = (FlinkExecutionContext) ec;
+	public void processInstruction(ExecutionContext ec)
+		throws DMLRuntimeException
+	{
+		FlinkExecutionContext flec = (FlinkExecutionContext)ec;
 
-		DataSet<Tuple2<MatrixIndexes, MatrixBlock>> A = fec.getBinaryBlockDataSetHandleForVariable(input1.getName());
-		DataSet<Tuple2<MatrixIndexes, MatrixBlock>> B = fec.getBinaryBlockDataSetHandleForVariable(input2.getName());
-		DataSet<Tuple2<MatrixIndexes, MatrixBlock>> out = A
-				.join(B)
-				.where(new ColumnSelector())
-				.equalTo(new RowSelector())
-				.with(new MultiplyMatrixBlocks())
-				.groupBy(new MatrixIndexesSelector())
-//				.combineGroup(new SumMatrixBlocksCombine())
-				.reduce(new SumMatrixBlocksStable());
+		String rddVar = (_type==CacheType.LEFT) ? input2.getName() : input1.getName();
+		String bcastVar = (_type==CacheType.LEFT) ? input1.getName() : input2.getName();
+		MatrixCharacteristics mcRdd = flec.getMatrixCharacteristics(rddVar);
+		MatrixCharacteristics mcBc = flec.getMatrixCharacteristics(bcastVar);
 
-		// register variable for output
-		fec.setDataSetHandleForVariable(output.getName(), out);
+		//get inputs
+		DataSet<Tuple2<MatrixIndexes,MatrixBlock>> in1 = flec.getBinaryBlockDataSetHandleForVariable( rddVar );
+
+		DataSet<Tuple2<MatrixIndexes, MatrixBlock>> in2 = flec.getBinaryBlockDataSetHandleForVariable(bcastVar);
+
+		//empty input block filter
+		if( !_outputEmpty )
+			in1 = in1.filter(new FilterNonEmptyBlocksFunction());
+
+		//execute mapmult instruction
+		DataSet<Tuple2<MatrixIndexes,MatrixBlock>> out = null;
+		if( requiresFlatMapFunction(_type, mcBc) ) {
+			out = in1.flatMap(new RDDFlatMapMMFunction(_type)).withBroadcastSet(in2, "bcastVar");
+		}
+		else if( preservesPartitioning(mcRdd, _type) ) {
+			out = in1.flatMap(new RDDMapMMPartitionFunction(_type)).withBroadcastSet(in2, "bcastVar");
+		}
+		else {
+			out = in1.map(new RDDMapMMFunction(_type)).withBroadcastSet(in2, "bcastVar");
+		}
+
+		//empty output block filter
+		if( !_outputEmpty )
+			out = out.filter(new FilterNonEmptyBlocksFunction());
+
+
+		//perform aggregation if necessary and put output into symbol table
+		if( _aggtype == SparkAggType.SINGLE_BLOCK ) //TODO: Test this case
+		{
+			MatrixBlock out2 = DataSetAggregateUtils.sumStable1(out);
+
+			//put output block into symbol table (no lineage because single block)
+			//this also includes implicit maintenance of matrix characteristics
+			flec.setMatrixOutput(output.getName(), out2);
+		}
+		else //MULTI_BLOCK or NONE
+		{
+			if( _aggtype == SparkAggType.MULTI_BLOCK ) {
+				out = out.groupBy(new MatrixMultiplicationFunctions.MatrixIndexesSelector())
+					.reduce(new MatrixMultiplicationFunctions.SumMatrixBlocksStable());
+			}
+
+			//put output RDD handle into symbol table
+			flec.setDataSetHandleForVariable(output.getName(), out);
+			flec.addLineageDataSet(output.getName(), rddVar);
+
+			//update output statistics if not inferred
+			updateBinaryMMOutputMatrixCharacteristics(flec, true);
+		}
+	}
+
+	/**
+	 *
+	 * @param mcIn
+	 * @param type
+	 * @return
+	 */
+	private static boolean preservesPartitioning( MatrixCharacteristics mcIn, CacheType type )
+	{
+		if( type == CacheType.LEFT )
+			return mcIn.dimsKnown() && mcIn.getRows() <= mcIn.getRowsPerBlock();
+		else // RIGHT
+			return mcIn.dimsKnown() && mcIn.getCols() <= mcIn.getColsPerBlock();
+	}
+
+	/**
+	 *
+	 * @param type
+	 * @param mcBc
+	 * @return
+	 */
+	private static boolean requiresFlatMapFunction( CacheType type, MatrixCharacteristics mcBc)
+	{
+		return    (type == CacheType.LEFT && mcBc.getRows() > mcBc.getRowsPerBlock())
+			|| (type == CacheType.RIGHT && mcBc.getCols() > mcBc.getColsPerBlock());
+	}
+
+	/**
+	 *
+	 *
+	 */
+	private static class RDDMapMMFunction extends RichMapFunction<Tuple2<MatrixIndexes, MatrixBlock>, Tuple2<MatrixIndexes, MatrixBlock>>
+	{
+		private CacheType _type = null;
+		private HashMap<Long, HashMap<Long, MatrixBlock>> _pbc = null;
+
+		//created operator for reuse
+		AggregateOperator agg = new AggregateOperator(0, Plus.getPlusFnObject());
+		AggregateBinaryOperator _op = new AggregateBinaryOperator(Multiply.getMultiplyFnObject(), agg);
+
+		public RDDMapMMFunction( CacheType type )
+		{
+			_type = type;
+		}
+
+		public RDDMapMMFunction()
+		{
+
+		}
+
+		@Override
+		public void open(Configuration parameters) throws Exception {
+			_pbc = new HashMap<Long, HashMap<Long, MatrixBlock>>();
+
+			Collection<Tuple2<MatrixIndexes,MatrixBlock>> blocklist = getRuntimeContext().getBroadcastVariable("bcastVar");
+
+			for (Tuple2<MatrixIndexes,MatrixBlock> broadcastTuple : blocklist){
+				long columnIndex = broadcastTuple.f0.getColumnIndex();
+				long rowIndex = broadcastTuple.f0.getRowIndex();
+
+				HashMap<Long, MatrixBlock> tempMap = _pbc.get(rowIndex);
+				if (tempMap == null) {
+					tempMap = new HashMap<Long, MatrixBlock>();
+				}
+				tempMap.put(columnIndex, broadcastTuple.f1);
+				_pbc.put(rowIndex, tempMap);
+			}
+		}
+
+		public void close() throws Exception {
+			for (Map.Entry<Long,HashMap<Long,MatrixBlock>> e : _pbc.entrySet()) {
+				e.getValue().clear();
+			}
+			_pbc.clear();
+		}
+
+		@Override
+		public Tuple2<MatrixIndexes, MatrixBlock> map( Tuple2<MatrixIndexes, MatrixBlock> arg0 )
+			throws Exception
+		{
+			MatrixIndexes ixIn = arg0.f0;
+			MatrixBlock blkIn = arg0.f1;
+
+			MatrixIndexes ixOut = new MatrixIndexes();
+			MatrixBlock blkOut = new MatrixBlock();
+
+			if( _type == CacheType.LEFT )
+			{
+
+				//get the right hand side matrix
+				MatrixBlock left = _pbc.get(1L).get(ixIn.getRowIndex());
+
+				//execute matrix-vector mult
+
+				OperationsOnMatrixValues.performAggregateBinary(
+					new MatrixIndexes(1L, ixIn.getRowIndex()), left, ixIn, blkIn, ixOut, blkOut, _op);
+			}
+			else //if( _type == CacheType.RIGHT )
+			{
+				//get the right hand side matrix
+				MatrixBlock right = _pbc.get(ixIn.getColumnIndex()).get(1L);
+
+				//execute matrix-vector mult
+				OperationsOnMatrixValues.performAggregateBinary(
+					ixIn, blkIn, new MatrixIndexes(ixIn.getColumnIndex(), 1L), right, ixOut, blkOut, _op);
+			}
+
+			//output new tuple
+			return new Tuple2<MatrixIndexes, MatrixBlock>(ixOut, blkOut);
+		}
+	}
+
+	/**
+	 *
+	 */
+	private static class RDDMapMMPartitionFunction extends RichFlatMapFunction<Tuple2<MatrixIndexes, MatrixBlock>, Tuple2<MatrixIndexes, MatrixBlock>>
+	{
+		private CacheType _type = null;
+		private HashMap<Long, HashMap<Long, MatrixBlock>> _pbc = null;
+
+		//created operator for reuse
+		AggregateOperator agg = new AggregateOperator(0, Plus.getPlusFnObject());
+		AggregateBinaryOperator _op = new AggregateBinaryOperator(Multiply.getMultiplyFnObject(), agg);
+
+		public RDDMapMMPartitionFunction( CacheType type)
+		{
+			_type = type;
+		}
+
+		public RDDMapMMPartitionFunction()
+		{
+
+		}
+
+		@Override
+		public void open(Configuration parameters) throws Exception {
+			_pbc = new HashMap<Long, HashMap<Long, MatrixBlock>>();
+
+			Collection<Tuple2<MatrixIndexes,MatrixBlock>> blocklist = getRuntimeContext().getBroadcastVariable("bcastVar");
+
+			for (Tuple2<MatrixIndexes,MatrixBlock> broadcastTuple : blocklist){
+				long columnIndex = broadcastTuple.f0.getColumnIndex();
+				long rowIndex = broadcastTuple.f0.getRowIndex();
+
+				HashMap<Long, MatrixBlock> tempMap = _pbc.get(rowIndex);
+				if (tempMap == null) {
+					tempMap = new HashMap<Long, MatrixBlock>();
+				}
+				tempMap.put(columnIndex, broadcastTuple.f1);
+				_pbc.put(rowIndex, tempMap);
+			}
+		}
+
+		public void close() throws Exception {
+			for (Map.Entry<Long,HashMap<Long,MatrixBlock>> e : _pbc.entrySet()) {
+				e.getValue().clear();
+			}
+			_pbc.clear();
+		}
+
+		@Override
+		public void flatMap(Tuple2<MatrixIndexes, MatrixBlock> arg0, Collector<Tuple2<MatrixIndexes, MatrixBlock>> out)
+			throws Exception
+		{
+			MatrixIndexes ixIn = arg0.f0;
+			MatrixBlock blkIn = arg0.f1;
+			MatrixBlock blkOut = new MatrixBlock();
+
+			if( _type == CacheType.LEFT )
+			{
+				//get the right hand side matrix
+				MatrixBlock left = _pbc.get(1L).get(ixIn.getRowIndex());
+				//execute index preserving matrix multiplication
+				left.aggregateBinaryOperations(left, blkIn, blkOut, _op);
+
+
+			}
+			else //if( _type == CacheType.RIGHT )
+			{
+				MatrixBlock right = _pbc.get(ixIn.getColumnIndex()).get(1L);
+
+				//execute index preserving matrix multiplication
+				blkIn.aggregateBinaryOperations(blkIn, right, blkOut, _op);
+
+			}
+
+			out.collect(new Tuple2<MatrixIndexes,MatrixBlock>(ixIn, blkOut));
+		}
+	}
+
+	/**
+	 *
+	 *
+	 */
+	private static class RDDFlatMapMMFunction extends RichFlatMapFunction<Tuple2<MatrixIndexes, MatrixBlock>,
+		Tuple2<MatrixIndexes, MatrixBlock>> {
+		private CacheType _type = null;
+		private HashMap<Long, HashMap<Long, MatrixBlock>> _pbc = null;
+
+		//created operator for reuse
+		AggregateOperator agg = new AggregateOperator(0, Plus.getPlusFnObject());
+		AggregateBinaryOperator _op = new AggregateBinaryOperator(Multiply.getMultiplyFnObject(), agg);
+
+		public RDDFlatMapMMFunction( CacheType type )
+		{
+			_type = type;
+		}
+
+		public RDDFlatMapMMFunction()
+		{
+
+		}
+
+		@Override
+		public void open(Configuration parameters) throws Exception {
+			_pbc = new HashMap<Long, HashMap<Long, MatrixBlock>>();
+
+			Collection<Tuple2<MatrixIndexes,MatrixBlock>> blocklist = getRuntimeContext().getBroadcastVariable("bcastVar");
+
+			for (Tuple2<MatrixIndexes,MatrixBlock> broadcastTuple : blocklist){
+				long columnIndex = broadcastTuple.f0.getColumnIndex();
+				long rowIndex = broadcastTuple.f0.getRowIndex();
+
+				HashMap<Long, MatrixBlock> tempMap = _pbc.get(rowIndex);
+				if (tempMap == null) {
+					tempMap = new HashMap<Long, MatrixBlock>();
+				}
+				tempMap.put(columnIndex, broadcastTuple.f1);
+				_pbc.put(rowIndex, tempMap);
+			}
+		}
+
+		public void close() throws Exception {
+			for (Map.Entry<Long,HashMap<Long,MatrixBlock>> e : _pbc.entrySet()) {
+				e.getValue().clear();
+			}
+			_pbc.clear();
+		}
+
+		@Override
+		public void flatMap( Tuple2<MatrixIndexes, MatrixBlock> arg0, Collector<Tuple2<MatrixIndexes, MatrixBlock>> out)
+			throws Exception
+		{
+			MatrixIndexes ixIn = arg0.f0;
+			MatrixBlock blkIn = arg0.f1;
+
+			if( _type == CacheType.LEFT )
+			{
+				//for all matching left-hand-side blocks
+				for(Map.Entry<Long,HashMap<Long,MatrixBlock>> i : _pbc.entrySet())
+				{
+					MatrixBlock left = i.getValue().get(ixIn.getRowIndex());
+
+					MatrixIndexes ixOut = new MatrixIndexes();
+					MatrixBlock blkOut = new MatrixBlock();
+
+					//execute matrix-vector mult
+					OperationsOnMatrixValues.performAggregateBinary(
+						new MatrixIndexes(i.getKey(), ixIn.getRowIndex()), left, ixIn, blkIn, ixOut, blkOut, _op);
+
+					out.collect(new Tuple2<MatrixIndexes, MatrixBlock>(ixOut, blkOut));
+				}
+			}
+			else //if( _type == CacheType.RIGHT )
+			{
+				//for all matching right-hand-side blocks
+				HashMap<Long,MatrixBlock> blocklist = _pbc.get(ixIn.getColumnIndex());
+
+				for(Map.Entry<Long,MatrixBlock> j : blocklist.entrySet()) {
+					//get the right hand side matrix
+					MatrixBlock right = j.getValue();
+					MatrixIndexes ixOut = new MatrixIndexes();
+					MatrixBlock blkOut = new MatrixBlock();
+
+					//execute matrix-vector mult
+					OperationsOnMatrixValues.performAggregateBinary(
+						ixIn, blkIn, new MatrixIndexes(ixIn.getColumnIndex(), j.getKey()), right, ixOut, blkOut, _op);
+
+					out.collect(new Tuple2<MatrixIndexes, MatrixBlock>(ixOut, blkOut));
+				}
+			}
+		}
 	}
 }
